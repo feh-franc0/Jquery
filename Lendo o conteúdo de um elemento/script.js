395d538c6a0ec8c042b9a9548218c7ca7de489b8
feh-franc0 <fernandofv1110@gmail.com>
2021-11-30 16:54:05 -0300

    Lendo o conteudo de um elemento

---
+++ Lendo o conteúdo de um elemento/script.js
@@ -0,0 +1,11 @@
+//qunaod usamos o text,é mostrado apenas o conteudo de texto
+console.log($("#lista1").text());
+
+//quando usamos o html,é mostrado o conteudo + os elementos html que o compõem
+console.log($("#lista1").html());
+
+//usamos o val para pegar o conteudo que esta dentro de um input
+console.log($("#novo").val());
+
+//Voce pode usar o attr para pegar os atributos de algum elemento,basta voce dizer qual atributo voce quer
+console.log($("#novo").attr("name"));
