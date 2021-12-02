c0f44a1fc7bea0c0bbdfceda40815a1d6b0adc89
feh-franc0 <fernandofv1110@gmail.com>
2021-12-02 19:26:47 -0300

    Removendo elementos

---
+++ Removendo elementos/script.js
@@ -0,0 +1,12 @@
+//removeu a lista
+//$("#lista1").remove()
+
+//removeu todos os elementos que estavam dentro da lista
+//$("#lista1").empty()
+
+//como remover apenas o item2 da nossa lista
+//$(".item2").remove()
+
+//se quiser tirar o item 2 e o item 4 de uma maneira mais simples, apenas use os parametros que no remove é permitido
+$("li").remove(".item2 , item4")
+
